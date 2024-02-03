446a661d030b56727b6810a2d19cb4fed07d345d
A <arpitjaiswal9526@gmail.com>
2024-02-03 13:59:21 +0530

    Working (but use your own api key)

--- open3.py
+++ open3.py
@@ -4,15 +4,31 @@ from langchain.indexes import VectorstoreIndexCreator
 import os
 from credapi import *
 from langchain_openai.chat_models import ChatOpenAI
+from pypdf import PdfReader
 
 
 os.environ["OPENAI_API_KEY"] = API
 
+pdflocation = input("enter pdf location: ")
 
-loader = TextLoader('data.txt')
+reader = PdfReader(pdflocation)
+number_of_pages = len(reader.pages)
+
+with open("da.txt", "w", encoding="utf-8") as f:
+    for i in range(number_of_pages):
+        page = reader.pages[i]
+        text = page.extract_text()
+        print(text)
+
+        f.write(text)
+
+filename = "da.txt"
+
+loader = TextLoader(filename, encoding="utf-8")
 # loader = DirectoryLoader(".", glob="*.txt")
 index = VectorstoreIndexCreator().from_loaders([loader])
 client = OpenAI(organization=OrgID, api_key=API)
+
 
 
 while True:
